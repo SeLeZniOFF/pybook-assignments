0ad7920435ccf7afaa23c7d63f140e4e7451de27
Илья Селезнев <46225413+SeLeZniOFF@users.noreply.github.com>
2020-10-25 00:10:48 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -3,19 +3,57 @@ import typing as tp
 
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
-    Encrypts plaintext using a Caesar cipher.
+    Encrypts a ciphertext using a Caesar cipher.
 
-    >>> encrypt_caesar("PYTHON")
+    >>> decrypt_caesar("PYTHON")
     'SBWKRQ'
-    >>> encrypt_caesar("python")
+    >>> decrypt_caesar("python")
     'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
+    >>> decrypt_caesar("Python3.6")
     'Sbwkrq3.6'
-    >>> encrypt_caesar("")
+    >>> decrypt_caesar("")
     ''
     """
+    j = 0
+
     ciphertext = ""
-    # PUT YOUR CODE HERE
+
+    for i in plaintext:
+
+        j += 1
+
+        if ord("A") <= ord(i) <= ord("Z"):
+
+            k = ord(i) + (shift % 26)
+
+            if k > ord("Z"):
+
+                k = ord("A") + k % 91
+
+                ciphertext += chr(k)
+
+            else:
+
+
+                ciphertext += chr(k)
+
+        elif ord("a") <= ord(i) <= ord("z"):
+
+            k = ord(i) + (shift % 26)
+
+            if k > ord("z"):
+
+                k = ord("a") + k % 123
+
+                ciphertext += chr(k)
+
+            else:
+
+
+                ciphertext += chr(k)
+        else:
+            ciphertext += i
+
     return ciphertext
 
 
@@ -33,7 +71,43 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+
+    for i in ciphertext:
+
+        if ord("A") <= ord(i) <= ord("Z"):
+
+            k = ord(i) - (shift % 26)
+
+            if k < ord("A"):
+
+                k = ord("Z") + (k - 64)
+
+                plaintext += chr(k)
+
+            else:
+
+                plaintext += chr(k)
+
+        elif ord("a") <= ord(i) <= ord("z"):
+
+            k = ord(i) - (shift % 26)
+
+            if k < ord("a"):
+
+                k = ord("z") + (k - 96)
+
+                plaintext += chr(k)
+
+            else:
+
+
+                plaintext += chr(k)
+        
+        else:
+
+
+            plaintext += i
+
     return plaintext
 
 
@@ -42,5 +116,7 @@ def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     Brute force breaking a Caesar cipher.
     """
     best_shift = 0
+
     # PUT YOUR CODE HERE
+    
     return best_shift
